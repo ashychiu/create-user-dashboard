aa65fd2821d9edaabc2a010b4d3adaced58a9dc7
ashychiu <ashychiu@gmail.com>
2022-05-27 09:25:11 +0800

    Update API URL to https

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -11,7 +11,7 @@ const UserList = () => {
   useEffect(() => {
     const fetchUserList = async () => {
       try {
-        const res = await fetch("http://jsonplaceholder.typicode.com/users");
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
         const userData = await res.json();
         if (sortType === "username") {
           userData.sort((a, b) => a.username.localeCompare(b.username));
