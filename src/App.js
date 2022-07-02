562094159a5d1a946495a02e84e63619e8d386a0
ashychiu <ashleychiu.codes@gmail.com>
2022-07-02 20:11:33 +0800

    Create BlogPosts & TransLink app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import UserList from "./components/UserList/UserList";
 import WeatherApp from "./components/WeatherApp/WeatherApp";
 import UserDetails from "./components/UserDetails/UserDetails";
+import TransLink from "./components/TransLink/TransLink";
+import BlogPosts from "./components/BlogPosts/BlogPosts";
+
 import "./styles/global.scss";
 
 function App() {
@@ -16,6 +19,8 @@ function App() {
             return <UserDetails {...routerProps} />;
           }}
         />
+        <Route path="/translink" component={TransLink} />
+        <Route path="/blog" component={BlogPosts} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/WeatherApp/WeatherApp.js
+++ src/components/WeatherApp/WeatherApp.js
@@ -1,24 +1,49 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./WeatherApp.scss";
 
 const WeatherApp = () => {
   const [data, setData] = useState([]);
+  const API_URL = "https://api.weather.gov/gridpoints/TOP/31,80/forecast";
 
   useEffect(() => {
     const fetchData = async () => {
+      await axios
+        .get(`${API_URL}`)
+        .then((response) => setData(response.data.properties.periods))
+        .catch((err) => console.error(err));
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const getResponse = async () => {
       try {
-        const res = await fetch(
-          "https://api.weather.gov/gridpoints/TOP/31,80/forecast"
-        );
-        const rawData = await res.json();
-        setData(rawData.properties.periods);
+        const response = await fetch(`${API_URL}`);
+        const rawData = await response.json();
+        console.log(rawData);
       } catch (err) {
         console.log(err);
       }
     };
-    fetchData();
+    getResponse();
   }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch(
+  //         "https://api.weather.gov/gridpoints/TOP/31,80/forecast"
+  //       );
+  //       const rawData = await res.json();
+  //       setData(rawData.properties.periods);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div key={data.number} className="container">
